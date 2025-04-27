d35deee0f8d245fb92317f60ee7f0d6e84a3b6d0
hydraslahir <tommy3667@live.ca>
2025-04-27 15:54:56 -0400

    Remove static reference to GUIStyle, given basic white is decent and ONGUI is required to generate the style

--- Utils/Editor/Common/EditorUtils.cs
+++ Utils/Editor/Common/EditorUtils.cs
@@ -5,24 +5,9 @@ namespace HYDRA
 {
     public static class EditorUtils
     {
-        static GUIStyle textureStyle;
-        static GUIStyle TextureStyle
-        {
-            get
-            {
-                if (textureStyle == null)
-                    textureStyle = new GUIStyle(GUI.skin.label)
-                    {
-                        alignment = TextAnchor.UpperCenter,
-                        fixedWidth = 70
-                    };
-                return textureStyle;
-            }
-        }
-
         public static void DrawTextureField(string name, TextureData data)
         {
-            GUILayout.Label(name, TextureStyle);
+            GUILayout.Label(name);
 
             GUILayout.BeginHorizontal();
             data.Texture = (Texture2D)EditorGUILayout.ObjectField(data.Texture, typeof(Texture2D), false, GUILayout.Width(70), GUILayout.Height(70));
